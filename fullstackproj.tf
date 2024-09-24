e472141fe7e3bec7176acd56e1fb4e02d08fb479
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-23 21:53:42 -0500

    Update fullstackproj.tf

--- fullstackproj.tf
+++ fullstackproj.tf
@@ -47,6 +47,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
 resource "aws_s3_object" "build_files" {
   for_each = fileset("/home/runner/work/fullstackProject/fullstackProject/codebase/rdicidr-0.1.0/build", "**")
+  bucket = aws_s3_bucket.app_bucket.bucket
   key    = "each.value"
   source = "/home/runner/work/fullstackProject/fullstackProject/codebase/rdicidr-0.1.0/build/${each.value}"
   acl = "public-read"
