8020ecc4d8630224a2c6060c7642d42c8700e7b8
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-09-23 21:38:13 -0500

    Update fullstackproj.tf

--- fullstackproj.tf
+++ fullstackproj.tf
@@ -34,9 +34,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
+
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.app_bucket.id
